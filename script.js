ab05cb1c64e1cb096cdca61b5f14b4b24a67c9e1
Thiago Silva <thiagowfer@gmail.com>
2022-12-17 08:21:10 -0300

    audio play and pause when click

--- script.js
+++ script.js
@@ -26,10 +26,18 @@ function loadSong(song){
 
 function playSong(){
     musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
 }
 
 function pauseSong(){
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
     
+    audio.pause()
 }
 
 // Event Listeners
