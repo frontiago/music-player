4669689580c8920b5e3d572a767823688ff5e520
Thiago Silva <thiagowfer@gmail.com>
2022-12-17 09:18:31 -0300

    Add click event to progressbar to set the current time of the song

--- script.js
+++ script.js
@@ -73,6 +73,26 @@ playBtn.addEventListener('click', () => {
     }
 })
 
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = currentTime / duration * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
 // Change songs
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
